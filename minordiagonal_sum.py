80ca3f6750ea6000d189f90a1c5a050d47e6f1f1
Harshitaa M S <88045719+msharshitaa@users.noreply.github.com>
2023-07-17 23:01:25 +0530

    Update minordiagonal_sum.py

--- minordiagonal_sum.py
+++ minordiagonal_sum.py
@@ -16,4 +16,13 @@ print(minor_diagonal_sum(matrix, row, col))
 """
 main diagonal means primary
 minor means secondary
+A00 A01 A02 A03
+A10 A11 A12 A13
+A20 A21 A22 A23
+A30 A31 A32 A33
+The primary diagonal is formed by the elements A00, A11, A22, A33. 
+Condition for Principal Diagonal: The row-column condition is row = column. 
+The secondary diagonal is formed by the elements A03, A12, A21, A30.
+Condition for Secondary Diagonal: The row-column condition is row = numberOfRows – column -1.
+refer(gfg)
 """
